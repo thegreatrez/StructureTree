e4be5f783a3ad8c026bdcd9c2da159caccb7893f
Rez <32952081+thegreatrez@users.noreply.github.com>
2024-02-28 18:47:38 +0330

    Update tree.php

--- tree.php
+++ tree.php
@@ -1,36 +1,43 @@
 <?php 
 
-function Tree($array,$depth = 0){
-     $FINAL = " └ ";
-     $ENTRY = " ├ " ;
-     $SKIP = " ┊ ";
-     $KEY = ": ";
-     $output = "";
-     if (is_array($array)){
-         $final = count($array) -1;
-         $current = 0;
-         foreach($array as $k=>$v){
-             foreach(range(0,$depth) as $i){
-                  if (!$depth == $i){
-                      $output .= $SKIP;
-                  }
-               }
-                  if ($current == $final){
-                      $output .= $FINAL;
-                  }else{
-                      $output .= $ENTRY;
-                       $current +=1;
-                  }
-                  if (is_array($v)){
-                      $output = $output . $k .$KEY. PHP_EOL . Tree($v, $depth +1 );
-                  }else{
-                      $output = $output . $k .$KEY. Tree($v, $depth +1 ).PHP_EOL;
-                  }
-            }   
-         }else{
-          $output = $array;
-     }
-   return $output;
+function Tree($array, $depth = 0){
+    $FINAL = " └ ";
+    $ENTRY = " ├ ";
+    $SKIP = " │ ";
+    $KEY = ": ";
+    $output = "";
+
+    if (is_array($array)){
+        $final = count($array) - 1;
+        $current = 0;
+
+        foreach($array as $k => $v){
+            for ($i = 0; $i <= $depth; $i++){
+                if ($i != $depth){
+                    $output .= $SKIP;
+                }
+                else {
+                    if ($current == $final){
+                        $output .= $FINAL;
+                    } else {
+                        $output .= $ENTRY;
+                        $current += 1;
+                    }
+                }
+            }
+
+            if (is_array($v)){
+                $output .= $k . $KEY . PHP_EOL . Tree($v, $depth + 1);
+            } else {
+                $output .= $k . $KEY . $v . PHP_EOL;
+            }
+        }
+    } else {
+        $output = $array;
+    }
+
+    return $output;
 }
 
 ?>
+
